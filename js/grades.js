73a4e2ecff93c820599f19db039655edfd6d3356
nerdsRob <robert@soundcloud.com>
2013-03-10 20:04:05 +0100

    Initial commit

---
+++ js/grades.js
@@ -0,0 +1,61 @@
+function Grade(numberOfSubjects) {
+	this.grades = new Array(numberOfSubjects);	
+	this.average = 1.0;
+	this.cash = 50.0;
+	this.name = "You";
+	
+	// Pre populate 
+	this.preFill = function() {
+		for (var i=0; i < this.grades.length; i++) {
+			this.grades[i] = 3;
+		}
+		this.cashIn();	
+	};
+	
+	// Dynamic selector for anims
+	this.print = function() {
+		return this.name;	
+	};
+	
+	// Helper
+	this.printGrades = function() {
+		return this.grades.toString();
+	};
+	
+	this.printAverage = function() {
+		return this.average.toPrecision(2).toString();	
+	};
+	
+	// Compute average grade
+	this.getAverage = function() {
+		var length = this.grades.length;
+		var sum = 0;
+		
+		for (var i=0; i<length; i++) {
+			sum += parseInt(this.grades[i]);
+		}
+		var average = sum/length;
+		this.average = average;
+	};
+	
+	// Compute reward
+	this.cashIn = function() {
+		var length = this.grades.length;
+		var reward = 0;
+		for (var i=0; i<length; i++) {
+			var grade = parseInt(this.grades[i]);
+			reward += this.reward(grade);
+		}
+		return this.cash + reward;
+	};
+	
+	this.reward = function(val) {
+		switch (val) {
+			case 1: return 10; 
+			case 2: return 5;
+			case 3: return 0;
+			case 4: return -5;
+		}	
+	};
+}
+	
